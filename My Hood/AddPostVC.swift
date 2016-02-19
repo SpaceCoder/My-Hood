8c91c85776f6766ab452c7f0c9012f9f64df0519
Anjam <anjam92@yahoo.com>
2016-02-18 21:12:08 -0600

    BugFix: deleted empty outlet

--- My Hood/AddPostVC.swift
+++ My Hood/AddPostVC.swift
@@ -19,13 +19,9 @@ class AddPostVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func CancelBtn(sender: AnyObject) {
         
